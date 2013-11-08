53e060199a9628552283ab37a7a9757e84a3130a
Rajesh Saravanan <rajesh.saravanan@thehindu.co.in>
2013-11-08 11:13:22 +0530

    Added brand colors to icons

--- js/script.js
+++ js/script.js
@@ -1,43 +1,44 @@
 $(document).ready(function(){
-  $('.apps').css({height: 396});
-  var scroll = false;
-  $('.apps').bind('mousewheel', function(e){
-        if(e.originalEvent.wheelDelta /120 > 0) {
-          
-        }
-        else{
-            if(!scroll){
-                $(".second-set").show();
-            $('.apps').css({height: 296}).addClass('overflow');
-            scroll =true; $(this).scrollTop(e.originalEvent.wheelDelta);
-            
-          }
-        }
-    });
-  
-  
-  $('.apps').scroll(function(){
-    var pos=$(this).scrollTop();
-    if(pos == 0){
-      scroll =false;
-      $('.apps').css({height: 396}).removeClass('overflow');
-       $(".second-set").hide();
-    }
-  });
-  
-  $('.apps .more').click(function(){
-    $(".second-set").show();
-    $(".apps").animate({ scrollTop: $('.apps')[0].scrollHeight}).css({height: 296}).addClass('overflow');
-    
-  });
-  
-  $(window).resize(function(){
-    console.log($(document).height());
-    $('.apps').css({maxHeight: $(window).height()});
-  });
-  
-  $(".button").click(function(){
-	$(".apps").toggle();
-  });
+
+	  var scroll = false;
+	  var launcherMaxHeight = 396;
+	  var launcherMinHeight = 296;
+	  
+	  $('.apps').bind('mousewheel', function(e){
+			if(e.originalEvent.wheelDelta /120 > 0) {
+			  
+			}
+			else{
+				if(!scroll){
+					$(".second-set").show();
+					$('.apps').css({height: launcherMinHeight}).addClass('overflow');
+					scroll =true; 
+					$(this).scrollTop(e.originalEvent.wheelDelta);
+				}
+			}
+		});
+	  
+	  
+	  $('.apps').scroll(function(){
+		var pos=$(this).scrollTop();
+		if(pos == 0){
+			scroll =false;
+			$('.apps').css({height: launcherMaxHeight}).removeClass('overflow');
+			$(".second-set").hide();
+		}
+	  });
+	  
+	  $('.apps .more').click(function(){
+		$(".second-set").show();
+		$(".apps").animate({ scrollTop: $('.apps')[0].scrollHeight}).css({height: 296}).addClass('overflow');
+	  });
+	  
+	  $(window).resize(function(){
+		$('.apps').css({maxHeight: $(window).height()});
+	  });
+	  
+	  $(".button").click(function(){
+		$(".app-launcher").toggle();
+	  });
   
 });
